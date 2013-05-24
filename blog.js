4100a7d73b09814bd2a80b34186d550ae0b6a41c
Benjamin Landers <benjaminrlanders@gmail.com>
2013-05-23 18:53:57 -0700

    deleted repetitive javascript

--- blog.js
+++
@@ -1,9 +0,0 @@
-function get(url) {
-   	return $.ajax({type: "GET", url: url, async: false}).responseText;
-}
-function addTo(name, msg){
-	$(name).html($(name).text()+msg);
-}
-function addToWithReturn(name, msg){
-	$(name).html($(name).html()+msg+"\n<br>\n");
-}
